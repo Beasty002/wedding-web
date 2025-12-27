3f773beb17ec4ac2326bb1a05481ab6448ad24c5
Beasty002 <xasbin12@gmail.com>
2025-12-27 21:41:46 +0545

    add animations

--- src/App.jsx
+++ src/App.jsx
@@ -58,18 +58,18 @@ function App() {
       <div className='w-[98dvw] mx-auto max-w-[1200px] pb-[200px]'>
         <section className='first-section w-full h-[330dvh] relative overflow-hidden'>
           <img src={topBar} alt="" className="w-full h-[600px] z-20 absolute top-0 left-0" />
-          <img src={roseRight} alt="" className="z-10 absolute top-[250px] left-[-20%] w-[50%] " />
-          <img src={roseLeft} alt="" className="z-10 absolute top-[250px] right-[-20%] w-[50%]" />
-          <img src={vinesflip} alt="" className="z-9 absolute top-[250px] left-[-5%] w-[50%] " />
-          <img src={vines} alt="" className="z-9 absolute top-[250px] right-[-5%] w-[50%]" />
+          <img src={roseRight} alt="" className="z-10 absolute top-[250px] left-[-20%] w-[50%] animate-float-gentle" />
+          <img src={roseLeft} alt="" className="z-10 absolute top-[250px] right-[-20%] w-[50%] animate-float-gentle-delayed" />
+          <img src={vinesflip} alt="" className="z-9 absolute top-[250px] left-[-5%] w-[50%] animate-float-gentle-delayed" />
+          <img src={vines} alt="" className="z-9 absolute top-[250px] right-[-5%] w-[50%] animate-float-gentle" />
           <img src={lordShiva} alt="" className="z-8 absolute w-[70%] top-[250px] left-1/2 -translate-x-1/2" />
           <img src={longPiece} alt="" className="z-7 h-full absolute right-[-2px]" />
           <img src={longSomething} alt="" className="z-6 h-full absolute left-0" />
-          <img src={castleCloud} alt="" className="z-20  w-[60%] top-[910px] absolute left-[18%]" />
-          <img src={baloon} alt="" className="z-7  w-[55%] top-[600px] absolute left-[5%]" />
-          <img src={tree2} alt="" className="z-4 h-[80%] absolute bottom-0 -left-[54%]" />
-          <img src={tree1} alt="" className="z-4 h-[70%] absolute bottom-0 -right-[52%]" />
-          <div className="z-3 w-[120%] absolute bottom-[-10%] left-1/2 -translate-x-1/2">
+          <img src={castleCloud} alt="" className="z-20  w-[60%] top-[910px] absolute left-[18%] animate-sway" />
+          <img src={baloon} alt="" className="z-7  w-[55%] top-[600px] absolute left-[5%] animate-balloon" />
+          <img src={tree2} alt="" className="z-4 h-[80%] absolute bottom-0 -left-[54%] animate-tree-sway" />
+          <img src={tree1} alt="" className="z-4 h-[70%] absolute bottom-0 -right-[52%] animate-tree-sway-delayed" />
+          <div className="z-3 w-[120%] absolute bottom-[-10%] animate-scenery-float left-1/2">
             <img src={purposeScenery} alt="" className="w-full h-full object-cover" />
           </div>
           <img src={ganeshLeft} alt="" className="z-20 w-[25%] absolute  bottom-[-1%] right-[-2%]" />
