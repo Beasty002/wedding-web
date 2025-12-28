7c16806c93bcf115e3790de97a67acbe2049d4e4
Beasty002 <xasbin12@gmail.com>
2025-12-28 21:26:28 +0545

    feat: responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ function App() {
   return (
     <div>
       <div className='w-[98dvw] mx-auto max-w-[1200px] pb-[200px]'>
-        <section className='first-section w-full h-[330dvh] relative overflow-hidden'>
+        <section className='first-section w-full relative overflow-hidden'>
           <img src={topBar} alt="" className="w-full h-[600px] z-20 absolute top-0 left-0" />
           <span className='hero-names z-20 absolute top-[30px] left-1/2 -translate-x-1/2 text-center'>
             Abhishek
@@ -84,15 +84,15 @@ function App() {
             <br />
             Kanika
           </span>
-          <img src={roseRight} alt="" className="z-10 absolute top-[250px] left-[-20%] w-[50%] animate-float-gentle" />
-          <img src={roseLeft} alt="" className="z-10 absolute top-[250px] right-[-20%] w-[50%] animate-float-gentle-delayed" />
-          <img src={vinesflip} alt="" className="z-9 absolute top-[250px] left-[-5%] w-[50%] animate-float-gentle-delayed" />
-          <img src={vines} alt="" className="z-9 absolute top-[250px] right-[-5%] w-[50%] animate-float-gentle" />
-          <img src={lordShiva} alt="" className="z-8 absolute w-[70%] top-[250px] left-1/2 -translate-x-1/2" />
+          <img src={roseRight} alt="" className="z-10 absolute top-5-hovers left-[-20%] w-[50%] animate-float-gentle" />
+          <img src={roseLeft} alt="" className="z-10 absolute top-5-hovers right-[-20%] w-[50%] animate-float-gentle-delayed" />
+          <img src={vinesflip} alt="" className="z-9 absolute top-5-hovers left-[-5%] w-[50%] animate-float-gentle-delayed" />
+          <img src={vines} alt="" className="z-9 absolute top-5-hovers right-[-5%] w-[50%] animate-float-gentle" />
+          <img src={lordShiva} alt="" className="z-8 absolute w-[70%] top-5-hovers left-1/2 -translate-x-1/2" />
           <img src={longPiece} alt="" className="z-7 h-full absolute right-[-2px]" />
           <img src={longSomething} alt="" className="z-6 h-full absolute left-0" />
-          <img src={castleCloud} alt="" className="z-20  w-[60%] top-[910px] absolute left-[18%] animate-sway" />
-          <img src={baloon} alt="" className="z-7  w-[55%] top-[600px] absolute left-[5%] animate-balloon" />
+          <img src={castleCloud} alt="" className="z-20  absolute left-[18%] animate-sway first-castle" />
+          <img src={baloon} alt="" className="z-7  absolute left-[5%] animate-balloon first-baloon" />
           <img src={tree2} alt="" className="z-4 h-[80%] absolute bottom-0 -left-[54%] animate-tree-sway" />
           <img src={tree1} alt="" className="z-4 h-[70%] absolute bottom-0 -right-[52%] animate-tree-sway-delayed" />
           <div className="z-3 w-[120%] absolute bottom-[-10%] animate-scenery-float left-1/2">
@@ -106,12 +106,12 @@ function App() {
 
 
 
-        <section className='second-section w-full h-[355dvh] relative overflow-hidden'>
+        <section className='second-section w-full relative overflow-hidden'>
           <img src={lordGanesh} alt="" className="w-[10%]  z-20 absolute top-24  left-1/2 -translate-x-1/2" />
-          <div className='invite-text pt-20'>
-            <img src={baloon} alt="" className="z-7  w-[50%] top-[20px] absolute right-[-70%]" />
-            <img src={moon} alt="" className="z-7  w-[45%] top-[210px] absolute left-[-60%]" />
-            <img src={castleCloud} alt="" className="z-7  w-[40%] top-[590px] absolute right-[-50%]" />
+          <img src={baloon} alt="" className="z-7  w-[25%] top-[20px] absolute second-balloon" />
+          <img src={moon} alt="" className="z-7  w-[25%] top-[210px] absolute second-moon" />
+          <img src={castleCloud} alt="" className="z-7  w-[25%] top-[800px] absolute second-castle" />
+          <div className='invite-text pt-20 '>
             <span>
             </span>
             <span>
@@ -219,13 +219,13 @@ function App() {
           </div>
 
         </section>
-        <section className="third-section w-full h-[350dvh] relative overflow-hidden">
+        <section className="third-section w-full relative overflow-hidden">
           <div className='pinhole-container absolute top-40 left-1/2 -translate-x-1/2'>
             <img src={pinholeinside} alt="" className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full animate-spin-variable ' />
             <img src={ringPinhole} alt="" className='absolute top-1/2 left-1/2 w-[80%] -translate-x-1/2 -translate-y-1/2' />
           </div>
 
-          <div className='bride-groom-section absolute top-[900px] w-full left-1/2 -translate-x-1/2'>
+          <div className='bride-groom-section absolute w-full left-1/2 -translate-x-1/2'>
             <img src={decor} className='w-20 mx-auto mb-4' />
             <div className='flex flex-col items-center text-center'>
               <span className='bride-groom-subtitle'>Meet the</span>
@@ -235,7 +235,7 @@ function App() {
           </div>
 
 
-          <div className='absolute w-full h-auto top-[1900px] left-1/2 -translate-x-1/2'>
+          <div className='carousel-parent absolute w-full h-auto left-1/2 -translate-x-1/2'>
             <div className='carousel-container absolute top-[-10px] left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full overflow-hidden'>
               <div className='carousel-track flex h-full'>
                 <img src={longShotCouple} alt="" className='h-full carousel-img object-cover shrink-0' />
@@ -249,13 +249,13 @@ function App() {
 
         </section>
 
-        <section className="fourth-section w-full h-[330dvh] relative overflow-hidden">
+        <section className="fourth-section w-full relative overflow-hidden">
           <div className='pinhole-container absolute top-40 left-1/2 -translate-x-1/2'>
             <img src={darkScroll} alt="" className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full animate-spin-variable ' />
             <img src={pinhole} alt="" className='absolute top-1/2 left-1/2 w-[80%] -translate-x-1/2 -translate-y-1/2' />
           </div>
 
-          <div className='things-section absolute top-[900px] w-full left-1/2 -translate-x-1/2'>
+          <div className='things-section absolute w-full left-1/2 -translate-x-1/2'>
             <img src={question} className='w-20 mx-auto mb-4' />
             <div className='flex flex-col items-center text-center'>
               <h1 className='things-title'>Things to<br />know</h1>
@@ -263,7 +263,7 @@ function App() {
             </div>
           </div>
 
-          <div className='info-grid absolute top-[1400px] left-1/2 -translate-x-1/2 w-[90%] max-w-[800px]'>
+          <div className='info-grid absolute left-1/2 -translate-x-1/2 w-[90%] max-w-[800px]'>
             <div className='grid grid-cols-2 gap-8'>
               <div className='info-card'>
                 <img src={instaIcon} alt="Hashtag" className='w-16 mx-auto mb-3' />
@@ -291,12 +291,12 @@ function App() {
             </div>
           </div>
         </section>
-        <section className="fifth-section w-full h-[150dvh] relative overflow-hidden">
+        <section className="fifth-section w-full h-[1000px] relative overflow-hidden">
           <div className='pinhole-container absolute top-40 left-1/2 -translate-x-1/2'>
             <img src={whiteScroll} alt="" className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full animate-spin-variable ' />
             <img src={pinhole} alt="" className='absolute top-1/2 left-1/2 w-[80%] -translate-x-1/2 -translate-y-1/2' />
           </div>
-          <h1 className='countdown absolute top-[800px] left-1/2 -translate-x-1/2 text-4xl font-bold text-center'>
+          <h1 className='countdown absolute left-1/2 -translate-x-1/2 text-4xl font-bold text-center'>
             The countdown begins
             <br />
             72D 05H 11M
